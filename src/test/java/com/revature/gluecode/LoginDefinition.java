f265e3555d317f0efc081b507257c0eab44be484
Bach Tran <bach.tran@revature.com>
2021-09-21 16:45:07 -0500

    Remove implementations from step definition classes

--- src/test/java/com/revature/gluecode/LoginDefinition.java
+++ src/test/java/com/revature/gluecode/LoginDefinition.java
@@ -12,37 +12,32 @@ public class LoginDefinition {
 	
 	@Given("I am at the homepage")
 	public void i_am_at_the_homepage() {
-		TestRunner.driver.get("http://demo.guru99.com/test/newtours/");
+
 	}
 
 	@When("I type into the username input with the value {string}")
 	public void i_type_into_the_username_input_with_the_value(String string) {
-		TestRunner.homePage.usernameInput().sendKeys(string);
+
 	}
 
 	@When("I type into the password input with the value {string}")
 	public void i_type_into_the_password_input_with_the_value(String string) {
-		TestRunner.homePage.passwordInput().sendKeys(string);
+
 	}
 
 	@When("I click the submit button")
 	public void i_click_the_submit_button() {
-		TestRunner.homePage.submitButton().click();
+
 	}
 
 	@Then("I should see the element for unsuccessful login")
 	public void i_should_see_the_element_for_unsuccessful_login() {
-		String actualText = TestRunner.homePage.unsuccessfulLoginElement().getText();
-		actualText = actualText.trim();
-		
-		assertEquals(actualText, "Enter your userName and password correct");
+
 	}
 
 	@Then("I should see the element for successful login")
 	public void i_should_see_the_element_for_successful_login() {
-		String actualText = TestRunner.landingPage.loginSuccessfulHeader().getText();
-		
-		assertEquals(actualText, "Login Successfully");
+
 	}
 	
 }
--- src/test/java/com/revature/gluecode/LogoutDefinition.java
+++ src/test/java/com/revature/gluecode/LogoutDefinition.java
@@ -11,14 +11,12 @@ public class LogoutDefinition {
 	
 	@When("I click on the signoff button")
 	public void i_click_on_the_signoff_button() {		
-		TestRunner.landingPage.signoffButton().click();
+	
 	}
 
 	@Then("I should see the homepage")
 	public void i_should_see_the_homepage() {
-		String currentUrl = TestRunner.driver.getCurrentUrl();
-		
-		assertEquals(currentUrl, "http://demo.guru99.com/test/newtours/index.php");
+
 	}
 	
 }
